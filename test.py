06ed90b8865704f8e1b2c26b6efbf4605069cb02
BeTheBeast <69687436+BeTheBeast@users.noreply.github.com>
2022-01-31 21:06:10 -0500

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-def single_true(iterable):
-    i = iter(iterable)
-    return any(i) and not any(i)  # check for first true : then make sure no other true values
-
-
-print(single_true([True, True]))
